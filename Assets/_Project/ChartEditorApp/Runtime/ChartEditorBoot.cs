7cc630188691d205665dc0cea59d97185a77968f
munkielol <linlu6561@gmail.com>
2026-03-08 04:16:52 -0700

    Add boot scripts

---
+++ Assets/_Project/ChartEditorApp/Runtime/ChartEditorBoot.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace RhythmicFlow.ChartEditorApp
+{
+    public sealed class ChartEditorBoot : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("[ChartEditorBoot] Awake");
+        }
+
+        private void Start()
+        {
+            Debug.Log("[ChartEditorBoot] Start");
+        }
+    }
+}
---
+++ Assets/_Project/Player/Runtime/PlayerBoot.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace RhythmicFlow.Player
+{
+    public sealed class PlayerBoot : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.Log("[PlayerBoot] Awake");
+        }
+
+        private void Start()
+        {
+            Debug.Log("[PlayerBoot] Start");
+        }
+    }
+}
